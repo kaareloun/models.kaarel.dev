a1848a5695bef648514e397629f842aa9e10daa9
Kaarel <kaareloun@gmail.com>
2026-08-31 00:01:30 +0300

    fix: error

--- src/serverFunctions/getOpenRouterModels.ts
+++ src/serverFunctions/getOpenRouterModels.ts
@@ -2,9 +2,8 @@ import { createServerFn } from "@tanstack/react-start";
 import { fetchOpenRouterModels } from "~/openrouter/fetchModels";
 import { startBackgroundScheduler } from "~/openrouter/scheduler";
 
-startBackgroundScheduler();
-
 export const getOpenRouterModels = createServerFn().handler(async () => {
+  startBackgroundScheduler();
   const models = await fetchOpenRouterModels();
 
   return models;
